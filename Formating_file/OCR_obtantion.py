84373d7f0ce9ab5dbc900e0c6c84ea83947e31b8
Nil <nilbiescas3@gmail.com>
2023-05-21 19:52:53 +0200

    Update of the ocr function

--- Formating_file/OCR_obtantion.py
+++ Formating_file/OCR_obtantion.py
@@ -1,27 +1,26 @@
 import os
 import easyocr
 
-def obtain_ocr(reader, train = True):
+def obtain_ocr(train = True):
     if train:
         variable = 'train'
     else:
         variable = 'test'
 
     ocr_file_path  = f'/home/xnmaster/data/ocr_{variable}.txt'
-    new_path_train = f'/home/xnmaster/data/{variable}.txt'
-    dir_images      = '/home/xnmaster/data/JPEGImages/'
-    with open(ocr_file_path, 'w') as ocr_file, open(new_path_train, 'r') as train_file:
+    path_1         = f'/home/xnmaster/data/{variable}.txt'
+    dir_images     = '/home/xnmaster/data/JPEGImages/'
+    with open(ocr_file_path, 'w') as ocr_file, open(path_1, 'r') as train_file:
         for line in train_file:
             img_name, label = line.split()
             file_path = os.path.join(dir_images, img_name + '.jpg')
-            ocr_output = ''
-            for text in reader.readtext(file_path):
-                ocr_output += ' ' + text[1]
-            if len(ocr_output) < 2:
-                ocr_file.write('0' + '\n')
+            ocr_output = reader.readtext(file_path, detail = 0, paragraph= True)
+            if len(ocr_output) == 0:
+                ocr_output = '0'
             else:
-                ocr_file.write(ocr_output + '\n')
+                ocr_output = ocr_output[0]
+            ocr_file.write(ocr_output + '\n')
 
-#reader = easyocr.Reader(['en'], gpu=True)
-#obtain_ocr(reader, True)
-#obtain_ocr(reader, False)
+reader = easyocr.Reader(['en'], gpu=True)
+obtain_ocr(reader, True)
+obtain_ocr(reader, False)
--- Formating_file/Train_and_Test.py
+++ Formating_file/Train_and_Test.py
@@ -1,11 +1,11 @@
 import os
 
-def create_new_file(new_file_path, type):
-    train_test_dir = '/home/xnmaster/data/ImageSets/0'
+def TrainTest_files(new_file_path, type):
+    train_test_dir = '/home/xnmaster/data/ImageSets/0/'
     class_label = 0
 
     with open(new_file_path, 'w') as new_file:
-        for i in range(1, 29):
+        for i in range(0, 28):
             file_to_change = os.path.join(train_test_dir, str(i) + f'_{type}.txt')
 
             with open(file_to_change, 'r') as file:
@@ -19,6 +19,6 @@ def create_new_file(new_file_path, type):
 new_path_train = '/home/xnmaster/data/train.txt'
 new_path_test  = '/home/xnmaster/data/test.txt'
 
-create_new_file(new_path_train, 'train')
-create_new_file(new_path_test, 'test')
+TrainTest_files(new_path_train, 'train')
+TrainTest_files(new_path_test,  'test')
 
