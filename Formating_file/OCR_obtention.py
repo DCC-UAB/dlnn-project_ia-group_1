cc2f8a6689de98fb365b79b3b3146597dc147171
JLC301103 <119518500+JLC301103@users.noreply.github.com>
2023-05-22 15:18:57 +0200

    Update OCR_obtention.py
    Just adding some comments, no actual changes

--- Formating_file/OCR_obtention.py
+++ Formating_file/OCR_obtention.py
@@ -7,10 +7,10 @@ def obtain_ocr(train = True):
     else:
         variable = 'test'
 
-    ocr_file_path  = f'/home/xnmaster/data/ocr_{variable}.txt'
-    path_1         = f'/home/xnmaster/data/{variable}.txt'
+    ocr_file_path  = f'/home/xnmaster/data/ocr_{variable}.txt' #If train is true the file will be saved as ocr_train.txt and if not as ocr_test.txt
+    path_1         = f'/home/xnmaster/data/{variable}.txt' #If train is true the file will be saved as train.txt and if not as test.txt
     dir_images     = '/home/xnmaster/data/JPEGImages/'
-    with open(ocr_file_path, 'w') as ocr_file, open(path_1, 'r') as train_file:
+    with open(ocr_file_path, 'w') as ocr_file, open(path_1, 'r') as train_file: # Open ocr file and train path in writing and reading respectively
         for line in train_file:
             img_name = line.split()[0]
             file_path = os.path.join(dir_images, img_name + '.jpg')
