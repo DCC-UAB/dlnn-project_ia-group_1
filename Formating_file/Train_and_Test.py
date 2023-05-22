553348ad0c0ff2489fab5b8da8029b855a637047
JLC301103 <119518500+JLC301103@users.noreply.github.com>
2023-05-22 15:21:54 +0200

    Update Train_and_Test.py
    Some quick commenting.

--- Formating_file/Train_and_Test.py
+++ Formating_file/Train_and_Test.py
@@ -1,14 +1,14 @@
 import os
 
 def TrainTest_files(new_file_path, type):
-    train_test_dir = '/home/xnmaster/data/ImageSets/0/'
+    train_test_dir = '/home/xnmaster/data/ImageSets/0/' 
     class_label = 0
 
-    with open(new_file_path, 'w') as new_file:
+    with open(new_file_path, 'w') as new_file: #Open for writing
         for i in range(0, 28):
             file_to_change = os.path.join(train_test_dir, str(i) + f'_{type}.txt')
 
-            with open(file_to_change, 'r') as file:
+            with open(file_to_change, 'r') as file: #Open for reading
                 for line in file:
                     line = line.split()
                     if line[1] == '1':
