37b98bcb360ab18338a9e451212771e420ab560f
Nil Biescas Rue <nilbiescas3@gmail.com>
2023-05-23 09:58:55 +0200

    chanes in the ocr

--- Formating_file/OCR_keras.py
+++ Formating_file/OCR_keras.py
@@ -8,30 +8,28 @@ def image_information(train=True):
         variable = 'test'
 
     reader = keras_ocr.pipeline.Pipeline()
-    batch_size = 8
-    start_index = 0
     with open(f'/home/xnmaster/data/{variable}.txt', 'r') as file:
         images_name = file.readlines()
 
     images = [os.path.join('/home/xnmaster/data/JPEGImages/', image_name.split()[0] + '.jpg') for image_name in images_name]
-    size_images = len(images)
 
-    while start_index < size_images:
-        images_to_read = images[start_index:min(start_index + batch_size, size_images)]
-        output = reader.recognize(images_to_read)
-        start_index += batch_size
-        yield output
+    results = []
+    for image_path in images:
+        output = reader.recognize([image_path])
+        result = " ".join([ind[0] for ind in output[0]]) if len(output) != 0 else '0'
+        results.append(result)
+
+    return results
 
 
-def obtain_keras_ocr(train = True):
+def obtain_keras_ocr(train=True):
     if train:
         variable = 'train'
     else:
         variable = 'test'
+    results = image_information(train)
     with open(f'/home/xnmaster/data/keras_ocr_{variable}.txt', 'w') as file:
-        for output in image_information(train):
-            result_to_write  = [" ".join([ind[0] for ind in image_output]) if len(image_output) != 0 else '0' for image_output in output]
-            file.write('\n'.join(result_to_write))
+        file.write('\n'.join(results))
 
-obtain_keras_ocr(train = True)
-obtain_keras_ocr(train = False)
+obtain_keras_ocr(train=True)
+obtain_keras_ocr(train=False)
